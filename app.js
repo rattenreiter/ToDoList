48218871d7e2cd50d37a013ca85369d6630a223c
rattenreiter <martin.meier@hu-berlin.de>
2022-08-04 22:05:59 +0200

    Complete Database Integration for one List

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
 const app = express();
 
-app.use(bodyParser.urlencoded({  extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 // Insert ejs in our express render engine
 app.set('view engine', 'ejs');
@@ -16,39 +17,75 @@ let ejsObject = {
     items: []
 };
 
-    app.get("/", (req, res) => {
+// MONGOOSE SETUP
+// --------------------------------------------------------
+mongoose.connect('mongodb://localhost:27017/ToDoList_DB');
+const listSchema = {
+  _id: {
+    type: Number,
+    required: [true, 'ID should be given automatically as timeStamp at time of creation']
+  },
+  listItem: {
+    type: String,
+    required: [true, 'List Item should be inserted']
+  },
+};
+
+const List = mongoose.model('List', listSchema);
+const WorkList = mongoose.model('Worklist', listSchema);
+
+app.get("/", (req, res) => {
+    List.find({}, (err, foundItems) => {
         ejsObject.listTitle = date.getDate();
-        ejsObject.items = listItems;
+        ejsObject.items = foundItems;
         res.render('list', ejsObject);
-    });
+    });    
+});
 
-    app.get("/work", (req, res) => {
-        ejsObject.listTitle = date.getDate();
-        ejsObject.listTitle += '<br>Arbeitsliste';
-        ejsObject.items = workListItems;
+app.get("/work", (req, res) => {
+    WorkList.find({}, (err, foundWorkItems) => {
+        ejsObject.listTitle = date.getDate()+'<br>Arbeitsliste';
+        ejsObject.items = foundWorkItems;
         res.render('list', ejsObject);
-    });
+    });   
+});
 
-    app.get("/about", (req, res) => {
-        res.render('about');
-    });
+app.get("/about", (req, res) => {
+  res.render('about');
+});
 
-    app.post("/", (req, res) => {
+app.post("/", (req, res) => {
 
-        if (req.body.listButton.includes('Arbeitsliste')) {
-            workListItems.push(req.body.newItem);
-            res.redirect("/work");
-        } else {
-            listItems.push(req.body.newItem);
-            res.redirect("/");
-        }
-        // itemString = '<div class="item"><input type="checkbox"><p>';
-        // itemString += req.body.newItem;
-        // itemString += '</p></div>';
-        // listItems.push(itemString);
-        // ejsObject.items = listItems.join(""); 
+    if (req.body.listButton.includes('Arbeitsliste')) {
+        const item = new WorkList(
+            {
+                _id: Date.now(),
+                listItem: req.body.newItem
+            }
+        );
+        WorkList.create(item);
+        res.redirect("/work");
+    } else {
+        const item = new List(
+            {
+                _id: Date.now(),
+                listItem: req.body.newItem
+            }
+        );
+        List.create(item);
+        res.redirect("/");
+    }
+});
+
+app.post('/delete', (req, res) => {
+    List.findByIdAndRemove(req.body.doneItem, (err) => {
+        if (err) {
+            console.log(err);
+        } 
     });
+    res.redirect('/');
+});
 
 app.listen(3000, () => {
-    console.log('Server listens on Port 3000');
+  console.log('Server listens on Port 3000');
 });
